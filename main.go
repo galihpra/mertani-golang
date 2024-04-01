c9f35bee4aff55b1bd348482f08b6bedb107a681
Galih Prayoga <galihp83@gmail.com>
2024-04-01 21:09:54 +0700

    database configuration

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"mertani-golang/config"
+	"mertani-golang/utils/database"
+)
+
+func main() {
+	var dbConfig = new(config.DatabaseMysql)
+	if err := dbConfig.LoadFromEnv(); err != nil {
+		panic(err)
+	}
+
+	dbConnection, err := database.MysqlInit(*dbConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := database.MysqlMigrate(dbConnection); err != nil {
+		panic(err)
+	}
+}
